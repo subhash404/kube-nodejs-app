0c24d0745c316ebe505303145ee25842dd88f103
subhash403 <rathnasubhash3@gmail.com>
2020-06-30 03:04:01 +0530

    Ansible Updated

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var PORT = process.env.PORT || DEFAULT_PORT;
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello Insider\n');
+  res.send('Hello Insider How are you \n');
 });
 
 app.listen(PORT)
